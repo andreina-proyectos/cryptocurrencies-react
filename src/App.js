4d70fc227be36703eef84aed482a3e53d470f500
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-11-24 01:14:45 +0100

    Add card and cryptocurrency information

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from '@material-ui/core/styles';
+import cryptoLogo from  '../node_modules/cryptocurrency-icons/svg/color/kmd.svg';
+
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: 275,
+  },
+});
+
 
 function App() {
+  const classes = useStyles();
   const [cryptoData, setCryptoData] = useState([]);
 
   useEffect(() => {
@@ -22,7 +35,23 @@ function App() {
         <ul className="main__crypto-list">
           {cryptoData.map(crypto => {return (
             <li className="crypto-list__crypto">
-              <h3 className="crypto__name">{crypto.name}</h3>
+              <Card className={classes.card}>
+                <img src={cryptoLogo} alt="" className="crypto__logo"/>
+                <CardContent>
+                  <h3 className="crypto__name">{crypto.name}</h3>
+                  <p className="crypto__symbol">{crypto.symbol}</p>
+                  <p className="crypto__price-usd">$  {crypto.price_usd}</p>
+                  <p className="crypto__price-btc">₿  {crypto.price_btc}</p>
+                  <p className="crypto__market-cap">Market cap: $ {crypto.market_cap_usd}</p>
+                  <p className="crypto__percent1h">Change 1h: {crypto.percent_change_1h} %</p>
+                  <p className="crypto__percent24h">Change 24h: {crypto.percent_change_24h} %</p>    
+                </CardContent>
+              </Card>
+
+
+
+
+
             </li>
           )})}
         </ul>
