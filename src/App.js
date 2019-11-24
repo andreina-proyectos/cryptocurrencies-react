def430b5a7e194146571ec9e8df84f804f968822
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-11-24 15:24:22 +0100

    Create function to take color from json crypto icon library

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import { makeStyles } from '@material-ui/core/styles';
-// import cryptoLogo from  './cryptocurrency-icons/svg/color/';
+import cryptoJson from './cryptocurrency-icons/manifest.json';
 
 const useStyles = makeStyles({
   card: {
@@ -25,7 +25,7 @@ function App() {
   }, []);
 
   const getCrytoLogo = (symbolCurrency) => {
-    const defaultCryptoLogo = 'https://cdn0.iconfinder.com/data/icons/tiny-icons-1/100/tiny-07-512.png';
+    const defaultCryptoLogo = 'http://www.myiconfinder.com/uploads/iconsets/ad922adbaf0350613b28cec297798d40-loading.png';
     let cryptoLogoImage;
     try{
       cryptoLogoImage = require(`./cryptocurrency-icons/svg/color/${symbolCurrency.toLowerCase()}.svg`)
@@ -34,6 +34,13 @@ function App() {
       cryptoLogoImage = defaultCryptoLogo;
     }
     return cryptoLogoImage;
+ }
+
+ const getCryptoColor = (symbolCurrency) => {
+  let cryptoFounded = cryptoJson.find(function(element){
+    return element.symbol.toLowerCase() === symbolCurrency.toLowerCase();
+  })
+  return cryptoFounded? cryptoFounded.color : "#2e3148"; 
  }
 
   return (
@@ -47,14 +54,15 @@ function App() {
             <li key={crypto.id} className="crypto-list__crypto">
               <Card className={classes.card}>
                 <CardContent>
-                  <img src={getCrytoLogo(crypto.symbol)} alt="" className="crypto__logo"/>
+                  <img src={getCrytoLogo(crypto.symbol)} alt="" className="crypto__logo" width="50"/>
                   <h3 className="crypto__name">{crypto.name}</h3>
                   <p className="crypto__symbol">{crypto.symbol}</p>
                   <p className="crypto__price-usd">$  {crypto.price_usd}</p>
                   <p className="crypto__price-btc">₿  {crypto.price_btc}</p>
                   <p className="crypto__market-cap">Market cap: $ {crypto.market_cap_usd}</p>
                   <p className="crypto__percent1h">Change 1h: {crypto.percent_change_1h} %</p>
-                  <p className="crypto__percent24h">Change 24h: {crypto.percent_change_24h} %</p>    
+                  <p className="crypto__percent24h">Change 24h: {crypto.percent_change_24h} %</p>
+                  <p style={{color: getCryptoColor(crypto.symbol)}} className="soyelcolor">soy el color</p>  
                 </CardContent>
               </Card>
             </li>
